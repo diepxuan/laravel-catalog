a06026076a4ba4318e69a730c639eb293fc4e017
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-05 10:04:56 +0000

    Class "ComposerPackage" not found

--- helpers.php
+++ helpers.php
@@ -9,6 +9,9 @@ declare(strict_types=1);
  *   Tran Ngoc Duc <caothu91@gmail.com>
  */
 
+use SplFileInfo;
+use Composer\InstalledVersions as ComposerPackage;
+
 if (!function_exists("module_path")) {
     function module_path($package_name, $path = null)
     {
