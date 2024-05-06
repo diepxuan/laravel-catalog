8cd21f1b5765e902709c8a095ef9487b54e0f3fc
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-06 09:00:22 +0000

    require diepxuan/laravel-core

--- helpers.php
+++ helpers.php
@@ -3,27 +3,10 @@
 declare(strict_types=1);
 
 /*
- * Copyright © 2019 Dxvn, Inc. All rights reserved.
+ * @copyright  © 2019 Dxvn, Inc.
  *
- * © Tran Ngoc Duc <ductn@diepxuan.com>
- *   Tran Ngoc Duc <caothu91@gmail.com>
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-05-06 15:59:45
  */
-
-use Composer\InstalledVersions as ComposerPackage;
-
-if (!function_exists("module_path")) {
-    function module_path($package_name, $path = null)
-    {
-        $packagePath = new \SplFileInfo(ComposerPackage::getInstallPath($package_name));
-        $packagePath = $packagePath->isDir() ? $packagePath : new \SplFileInfo(__DIR__ . '/../');
-
-        if ($path) {
-            return Str::of($packagePath->getRealPath())
-                                ->explode(DIRECTORY_SEPARATOR)
-                                ->push(Str::of($path)->trim()->explode(DIRECTORY_SEPARATOR))
-                                ->flatten()
-                                ->implode(DIRECTORY_SEPARATOR);
-        }
-        return $packagePath->getRealPath();
-    }
-}
