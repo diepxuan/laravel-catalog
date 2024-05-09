874d1bee6756f78c2f61903e4ce0e47e272994d8
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-09 04:36:34 +0000

    sync products to magento

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,14 +18,69 @@
         tr:nth-child(even) {
             background-color: #dddddd;
         }
+
+        body {
+            counter-reset: Serial;
+            /* Set the Serial counter to 0 */
+        }
+
+        table {
+            border-collapse: separate;
+        }
+
+        tr td:first-child:before {
+            counter-increment: Serial;
+            /* Increment the Serial counter */
+            content: counter(Serial);
+            /* Display the counter */
+        }
+
+        button.sync {
+            border: solid 1px rgb(184, 63, 63);
+            border-radius: 10%;
+            color: rgb(184, 63, 63);
+        }
+
+        td a {
+            text-decoration-line: none;
+            color: rgb(10, 149, 22);
+        }
     </style>
     <table class="table table-hover table-condensed table-sm text-monospace small">
         <tbody>
+            {{-- @php
+                setlocale(LC_CTYPE, 'vi_VN');
+            @endphp --}}
+            {{-- @dd($mProducts) --}}
             @foreach ($products as $product)
-                {{-- @dd($product) --}}
+                {{-- @foreach (mb_list_encodings() as $chr)
+                    {{ mb_convert_encoding($product->ten_vt, 'UTF-8', $chr) . ' : ' . $chr }} <br>
+                @endforeach --}}
+                {{-- @dd($product, $product->ten_vt, mb_detect_encoding($product->ten_vt), iconv('UTF-8', 'ISO-8859-1//TRANSLIT', $product->ten_vt)) --}}
+                @php
+                    $mProduct = null;
+                    $mProduct = $mProducts->first(function ($item) use ($product) {
+                        return $item->sku == $product->ma_vt;
+                    });
+                @endphp
                 <tr id="{{ "$product->ma_cty|$product->ma_vt" }}">
+                    <td></td>
                     <td>{{ $product->ma_vt }}</td>
                     <td>{{ $product->ten_vt }}</td>
+                    <td>{{ $product->ma_nhvt }}</td>
+                    <td>
+                        @if (isset($mProduct))
+                            <a href="{{ "https://www.diepxuan.com/catalog/product/view/id/$mProduct->id" }}">magento</a>
+                        @else
+                            <form method="post" action="{{ route('catalog.store') }}">
+                                @method('POST') @csrf
+                                <input type="hidden" value="magento2" name="type" />
+                                <input type="hidden" value="{{ $product->ma_vt }}" name="sku" />
+                                <input type="hidden" value="{{ $product->ten_vt }}" name="name" />
+                                <button type="submit" class="sync">sync</button>
+                            </form>
+                        @endif
+                    </td>
                 </tr>
             @endforeach
         </tbody>
--- src/Http/Controllers/CatalogController.php
+++ src/Http/Controllers/CatalogController.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-05-06 22:15:01
+ * @lastupdate 2024-05-09 10:21:55
  */
 
 namespace Diepxuan\Catalog\Http\Controllers;
@@ -18,21 +18,38 @@ use Diepxuan\Catalog\Models\Product;
 use Diepxuan\Magento\Magento2 as Magento;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class CatalogController extends Controller
 {
+    /**
+     * Manager client.
+     *
+     * @var Diepxuan\Magento\Magento2
+     */
+    private $magento;
+
+    /**
+     * Catalog construct.
+     *
+     * @param Diepxuan\Magento\Magento2 $magento
+     */
+    public function __construct(Magento $magento)
+    {
+        $this->magento = new Magento();
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $products = Product::all();
-        $magento  = new Magento();
-        // dd(config());
-        // dd($magento->products()->get());
+        $products  = Product::all();
+        $mProducts = $this->magento->products()->get();
 
         return view('catalog::index', [
-            'products' => $products,
+            'products'  => $products,
+            'mProducts' => $mProducts,
         ]);
     }
 
@@ -47,7 +64,34 @@ class CatalogController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse {}
+    public function store(Request $request): RedirectResponse
+    {
+        $this->magento->products()->create([
+            'sku'               => $request->get('sku'),
+            'name'              => $request->get('name'),
+            'price'             => $request->get('price', 0),
+            'attribute_set_id'  => 4,
+            'status'            => 1,
+            'visibility'        => 4,
+            'type_id'           => 'simple',
+            'custom_attributes' => [
+                [
+                    'attribute_code' => 'meta_title',
+                    'value'          => $request->get('name'),
+                ],
+                [
+                    'attribute_code' => 'meta_keyword',
+                    'value'          => $request->get('name'),
+                ],
+                [
+                    'attribute_code' => 'meta_description',
+                    'value'          => $request->get('name'),
+                ],
+            ],
+        ]);
+
+        return Redirect::route('catalog.index');
+    }
 
     /**
      * Show the specified resource.
