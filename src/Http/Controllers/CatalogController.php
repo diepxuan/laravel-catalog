fff4481d96884ff2122e59f89e296b5fb9287839
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-29 00:07:33 +0700

    update product

--- src/Http/Controllers/CatalogController.php
+++ src/Http/Controllers/CatalogController.php
@@ -8,19 +8,17 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-05-27 20:13:44
+ * @lastupdate 2024-05-28 11:08:07
  */
 
 namespace Diepxuan\Catalog\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Diepxuan\Catalog\Models\Product;
-use Diepxuan\Magento\Magento;
 use Diepxuan\Simba\Models\Product as SProduct;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Str;
 
 class CatalogController extends Controller
 {
@@ -48,39 +46,6 @@ class CatalogController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $sku     = $request->get('sku');
-        $name    = $request->get('name');
-        $price   = $request->get('price', 0);
-        $url_key = Str::of(vn_convert_encoding($name))->lower()->replace(' ', '-');
-
-        Magento::products()->create([
-            'sku'               => $sku,
-            'name'              => $name,
-            'price'             => $price,
-            'attribute_set_id'  => 4,
-            'status'            => 1,
-            'visibility'        => 4,
-            'type_id'           => 'simple',
-            'custom_attributes' => [
-                [
-                    'attribute_code' => 'meta_title',
-                    'value'          => $name,
-                ],
-                [
-                    'attribute_code' => 'meta_keyword',
-                    'value'          => $name,
-                ],
-                [
-                    'attribute_code' => 'meta_description',
-                    'value'          => $name,
-                ],
-                [
-                    'attribute_code' => 'url_key',
-                    'value'          => $url_key,
-                ],
-            ],
-        ]);
-
         return Redirect::route('catalog.index');
     }
 
@@ -96,10 +61,8 @@ class CatalogController extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param mixed $id
      */
-    public function edit($id)
+    public function edit(Product $sanpham)
     {
         return view('catalog::product/edit');
     }
@@ -107,9 +70,21 @@ class CatalogController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param mixed $id
+     * @param mixed $request
+     * @param mixed $sanpham
      */
-    public function update(Request $request, $id): RedirectResponse {}
+    public function update(Request $request, Product $sanpham): RedirectResponse
+    {
+        $sanpham->sku      = $request->get('sku', $sanpham->sku);
+        $sanpham->name     = $request->get('name', $sanpham->name);
+        $sanpham->price    = $request->get('price', $sanpham->price);
+        $sanpham->category = $request->get('category', $sanpham->category);
+        $sanpham->quantity = $request->get('quantity', $sanpham->quantity ?: 0);
+        $sanpham->status   = $request->boolean('status');
+        $sanpham->save();
+
+        return Redirect::route('catalog.index');
+    }
 
     /**
      * Remove the specified resource from storage.
