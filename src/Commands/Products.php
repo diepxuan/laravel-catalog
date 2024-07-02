0130f476ef83093188205ce025aacba4741fdc29
Tran Ngoc Duc <caothu91@gmail.com>
2024-07-02 15:47:10 +0700

    fix update when schedule running

--- src/Commands/Products.php
+++ src/Commands/Products.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-06-21 11:05:08
+ * @lastupdate 2024-07-02 15:45:48
  */
 
 namespace Diepxuan\Catalog\Commands;
@@ -51,12 +51,12 @@ class Products extends Command
     public function productIntergration()
     {
         $this->output->writeln('[i] Starting import Simba products');
-
-        $sProducts = SProduct::withQuantity()->get();
+        $sProducts = SProduct::all();
         $total     = $sProducts->count();
-        $sProducts->map(function (SProduct $sProduct, int $index) use ($total) {
-            $status = $sProduct->product ? $sProduct->product->status : false;
-            $status = $sProduct->status && $status && $sProduct->price > 0;
+        $sProducts->map(function (SProduct $sProduct, int $index) use ($total): void {
+            $sProduct = SProduct::withQuantity()->where('ma_vt', $sProduct->sku)->get()->first();
+            $status   = $sProduct->product ? $sProduct->product->status : false;
+            $status   = $sProduct->status && $status && $sProduct->price > 0;
             $sProduct->product()->updateOrCreate([], [
                 'sku'      => $sProduct->sku,
                 'name'     => $sProduct->name,
@@ -70,73 +70,17 @@ class Products extends Command
             } else {
                 $this->output->writeln("[<fg=green>✔</>] Imported <fg=red>{$sProduct->product->sku}</> ({$index}/{$total})");
             }
-            // $this->output->writeln("    status [<fg=green>✓</>]");
+            // $this->output->writeln('    status [<fg=green>✓</>]');
             $this->output->writeln("    {$sProduct->product->name}");
             $this->output->writeln("    {$sProduct->product->quantity}");
-
-            return $sProduct;
         });
 
-        Product::all()->map(static function (Product $product) {
+        Product::all()->map(function (Product $product) {
             if ($product->sProduct) {
                 return $product;
             }
             $product->delete();
             $this->output->writeln("[<fg=red>✘</>] Deleted <fg=red>{$product->sku}</>");
         });
-
-        return;
-        $self   = $this;
-        $format = ' %current%/%max% [%bar%] %percent:3s%% %message%';
-
-        $self->output->writeln('[i] Loading all products');
-        $products = Product::all()->keyBy('sku');
-        $self->output->writeln(sprintf('[i] Loaded <fg=green>%s</> products.', $products->count()));
-
-        $self->output->writeln('[i] Starting import Simba products');
-        // $sProducts = SProduct::all()->keyBy('sku');
-        $sProducts = SProduct::withQuantity()->get()->keyBy('sku');
-        $self->withProgressBar($sProducts, static function ($sProduct, $progressBar) use ($products): void {
-            $product = $products->get($sProduct->sku);
-            if ($product) {
-                $product->name     = $sProduct->name;
-                $product->price    = $sProduct->price;
-                $product->category = $sProduct->category;
-                $product->status   = $sProduct->status && $product->status && $sProduct->price > 0;
-                $product->quantity = (float) $sProduct->quantity;
-                if ($product->isDirty()) {
-                    $product->save();
-                }
-            } else {
-                $product = Product::updateOrCreate(
-                    ['sku' => $sProduct->sku],
-                    [
-                        'name'     => $sProduct->name,
-                        'price'    => $sProduct->price,
-                        'category' => $sProduct->category,
-                        'status'   => $sProduct->status,
-                        'quantity' => (float) ($sProduct->quantity ?: 0),
-                    ]
-                );
-                $products->put($product->id, $products);
-            }
-            $progressBar->setMessage('');
-        });
-        $self->output->writeln("\r\n[i] Finished import Simba products");
-
-        $self->output->writeln('[i] Deleting missing products from Simba');
-        $self->withProgressBar($products, static function ($product, $progressBar) use ($sProducts, $products, $format): void {
-            $progressBar->setFormat($format);
-            $progressBar->setMessage(" {$product->sku}");
-
-            if (!$sProducts->get($product->sku)) {
-                $progressBar->setMessage(" {$product->sku} deleting");
-                $products->pull($product->id);
-                $product->delete();
-            }
-
-            $progressBar->setMessage('');
-        });
-        $self->output->writeln("\r\n[i] Finished delete missing products");
     }
 }
