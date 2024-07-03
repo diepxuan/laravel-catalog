8bab37c0a0aa627eb0c3ef0fb1d031d7a56e45dd
Tran Ngoc Duc <caothu91@gmail.com>
2024-07-03 11:12:00 +0700

    remove @deprecated functions

--- src/Providers/CatalogServiceProvider.php
+++ src/Providers/CatalogServiceProvider.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-06-16 07:08:02
+ * @lastupdate 2024-07-03 11:10:43
  */
 
 namespace Diepxuan\Catalog\Providers;
@@ -19,7 +19,6 @@ use Diepxuan\Catalog\Models\Category;
 use Diepxuan\Catalog\Models\Product;
 use Diepxuan\Catalog\Observers\CategoryObserver;
 use Diepxuan\Catalog\Observers\ProductObserver;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class CatalogServiceProvider extends ServiceProvider
@@ -48,42 +47,6 @@ class CatalogServiceProvider extends ServiceProvider
     }
 
     /**
-     * Register translations.
-     *
-     * @deprecated
-     */
-    public function registerTranslations(): void
-    {
-        $langPath = resource_path('lang/modules/' . $this->moduleNameLower);
-
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom($langPath);
-        } else {
-            $this->loadTranslationsFrom(module_path($this->moduleName, 'lang'), $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom(module_path($this->moduleName, 'lang'));
-        }
-    }
-
-    /**
-     * Register views.
-     *
-     * @deprecated
-     */
-    public function registerViews(): void
-    {
-        $viewPath   = resource_path('views/modules/' . $this->moduleNameLower);
-        $sourcePath = module_path($this->moduleName, 'resources/views');
-
-        $this->publishes([$sourcePath => $viewPath], ['views', $this->moduleNameLower . '-module-views']);
-
-        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
-
-        $componentNamespace = str_replace('/', '\\', $this->moduleName);
-        Blade::componentNamespace($componentNamespace, $this->moduleNameLower);
-    }
-
-    /**
      * Get the services provided by the provider.
      *
      * @return array<string>
@@ -91,58 +54,5 @@ class CatalogServiceProvider extends ServiceProvider
     public function provides(): array
     {
         return [];
-    }
-
-    /**
-     * Register commands in the format of Command::class.
-     *
-     * @deprecated
-     */
-    protected function registerCommands(): void
-    {
-        $this->commands([
-            CatalogSync::class,
-        ]);
-    }
-
-    /**
-     * Register command Schedules.
-     *
-     * @deprecated
-     */
-    protected function registerCommandSchedules(): void
-    {
-        // $this->app->booted(function () {
-        //     $schedule = $this->app->make(Schedule::class);
-        //     $schedule->command('inspire')->hourly();
-        // });
-    }
-
-    /**
-     * Register config.
-     *
-     * @deprecated
-     */
-    protected function registerConfig(): void
-    {
-        $this->publishes([module_path($this->moduleName, 'config/config.php') => config_path($this->moduleNameLower . '.php')], 'config');
-        $this->mergeConfigFrom(module_path($this->moduleName, 'config/config.php'), $this->moduleNameLower);
-    }
-
-    /**
-     * @return array<string>
-     *
-     * @deprecated
-     */
-    private function getPublishableViewPaths(): array
-    {
-        $paths = [];
-        foreach (config('view.paths') as $path) {
-            if (is_dir($path . '/modules/' . $this->moduleNameLower)) {
-                $paths[] = $path . '/modules/' . $this->moduleNameLower;
-            }
-        }
-
-        return $paths;
     }
 }
