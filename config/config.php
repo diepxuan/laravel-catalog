b87d9b9f45c5706d714453eb2cb920a758987a50
diepxuan <ductn@diepxuan.com>
2024-12-12 15:51:32 +0000

    create app layout

--- config/config.php
+++ config/config.php
@@ -1,5 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-12-12 22:19:44
+ */
+
+use Diepxuan\Catalog;
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-12-12 22:15:55
+ */
+
 return [
-    'name' => 'diepxuan/laravel-catalog',
+    'name'      => 'diepxuan/laravel-catalog',
+    'namespace' => Catalog::class,
 ];
