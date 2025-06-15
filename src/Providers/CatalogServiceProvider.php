e58d6e4fc1ed05ef3a76367a657b47d2ccb4d0ae
diepxuan <ductn@diepxuan.com>
2025-06-15 14:42:55 +0000

    add support for idx nixos

--- src/Providers/CatalogServiceProvider.php
+++ src/Providers/CatalogServiceProvider.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-05-24 16:46:18
+ * @lastupdate 2025-06-14 12:19:56
  */
 
 namespace Diepxuan\Catalog\Providers;
@@ -16,13 +16,18 @@ namespace Diepxuan\Catalog\Providers;
 use Diepxuan\Catalog\Commands\CatalogSync;
 use Diepxuan\Catalog\Commands\EnvTest;
 use Diepxuan\Catalog\Commands\Scavenger;
+use Diepxuan\Catalog\Connectors\SqlServerConnector;
+use Diepxuan\Catalog\Facades\CatalogFunctionsFacade;
 use Diepxuan\Catalog\Facades\CatalogServiceFacade;
 use Diepxuan\Catalog\Models\Category;
 use Diepxuan\Catalog\Models\Product;
 use Diepxuan\Catalog\Models\User;
 use Diepxuan\Catalog\Observers\CategoryObserver;
 use Diepxuan\Catalog\Observers\ProductObserver;
+use Diepxuan\Catalog\Services\CatalogFunctions;
 use Diepxuan\Catalog\Services\CatalogService;
+use Illuminate\Database\Connection;
+use Illuminate\Database\SqlServerConnection;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
@@ -35,6 +40,16 @@ class CatalogServiceProvider extends ServiceProvider
     {
         Category::observe(CategoryObserver::class);
         Product::observe(ProductObserver::class);
+
+        // Đăng ký connector tùy chỉnh của chúng ta
+        Connection::resolverFor('sqlsrv', static function ($connection, $database, $prefix, $config) {
+            $connector = new SqlServerConnector();
+
+            // \Debugbar::info($config);
+            $pdo = $connector->connect($config);
+
+            return new SqlServerConnection($pdo, $database, $prefix, $config);
+        });
     }
 
     /**
@@ -55,7 +70,9 @@ class CatalogServiceProvider extends ServiceProvider
         ]);
 
         $this->app->singleton('catalog-service', static fn ($app) => new CatalogService());
+        $this->app->singleton('catalog-functions', static fn ($app) => new CatalogFunctions());
         AliasLoader::getInstance()->alias('CatalogService', CatalogServiceFacade::class);
+        AliasLoader::getInstance()->alias('CatalogFunctions', CatalogFunctionsFacade::class);
     }
 
     /**
