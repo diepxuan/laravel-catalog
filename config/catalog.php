d1acf62b4412916a05c4328e010f1a874e689d45
diepxuan <ductn@diepxuan.com>
2026-02-23 07:52:38 +0000

    Merge pull request #78 from diepxuan/fix/command-registration-issues
    fix: Standardize package config files and prevent command registration issues

---
+++ config/catalog.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-12-12 22:19:44
+ */
+
+return [
+    'name'      => 'diepxuan/laravel-catalog',
+    'namespace' => 'Diepxuan\Catalog',
+];
